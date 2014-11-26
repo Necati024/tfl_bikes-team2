8f24e802d152c70f97a1c5abada19e37bbc56af1
Necati Munir <NecatiMunir@Fr.local>
2014-11-26 01:23:18 +0000

    data
    data added

--- nodetest1/routes/index.js
+++ nodetest1/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var detailarray=0;
 /* GET home page. */
 router.get('/', function(req, res) {
     res.render('index', { title: 'Express' });
@@ -17,34 +17,24 @@ var mongoose = require('mongoose')
   var  tableSchema =new mongoose.Schema({
      id:Number,
      name:String,
-     terminalName:String,
-     lat:String,
-     longd:Number,
-     installed:String,
-     locked:String,
-     installDate:Number,
-     removalDate:String,
-     temporary:String,
-     nbBikes:Number,
-     nbEmptyDocks:Number,
-     nbDocks:Number,
-     stations_Id:Number
-
-  })
-var  detailSchema= new mongoose.Schema({
      time:String,
-     amount:Number,
-     cap:Number,
-     persent:Number,
-     status:Number
-});
+     lat:String,
+     longd:String,
+     status:Number,
+     })
 
 
 
 
 var stationSchema = new mongoose.Schema({
-    stationName:String,
-    details:[detailSchema]
+    id:Number,
+    name:String,
+    lat:String,
+    longd:String,
+    amount:Number,
+    nbEmptyDocks:Number,
+    cap:Number,
+    time:String,
 });
 
 var Station=mongoose.model('bikeStation', stationSchema); 
@@ -57,67 +47,68 @@ var StationTable=mongoose.model('stationtable', tableSchema);
      StationTable:StationTable
      }
 
-var index=0;
-for(index=0; index<40; index++){
 
- var capacity=Math.round(Math.random() * 35);
- var a=Math.round(Math.random() * capacity);
- var pecent=a/capacity;
-  var s;
- if (pecent>0.85){
-     s=1;
- }
- else if (pecent<0.15){
-    s=-1;
- }
- else {
-    s=0;
- }
-
- var Stations = ["Kings Cross", "Euston", "EversholtStreet","Penton Road","Camden","Covent Graden","Baker Street","Picadilly"];
- var data =new Station({
-   stationName: Stations[Math.round(Math.random()*7)],
-   details:
-   [ {
-     time:1 ,
-     amount:a,
-     cap:capacity,
-     persent:pecent,
-     status:s
+StationTable.count({}, function( err, count){
+    console.log( "Number of users:", count );
+  if (count==0){
+ Station.find({},{},function(err, details) {
+  if (err) return console.error(err);
+    //console.dir(details);
+    console.dir(details);
+    for (var i in details) {
+      var percent= (details[i].amount)/(details[i].cap);
+     var  status;
+      
+     if (percent>0.85){
+      status=1;
+      }
+    else if (percent<0.15){
+      status=-1;
      }
-   ]
+    else {
+      status=0;
+    }
+    
+      addData(details[i].id,details[i].name,details[i].time,details[i].lat,details[i].longd,status)
+      
+ 
+}
 });
-  data.save(function(err, dat) {
+}
+});
+ 
+  function addData(p1,p2,p3,p4,p5,p6){
+
+     var data =new StationTable({
+      
+    
+       id:p1,
+      name:p2,
+      time:p3,
+      lat:p4,
+      longd:p5,
+      status:p6
+    });
+data.save(function(err, dat) {
   if (err) return console.error(err);
   console.dir(dat);
+
 });
+ 
 }
 
 
-router.get('/stationdetails', function(req, res) {
-  Station.find({},{},function(err,details){
-       if(err)return console.error(err);
-        
-       res.render('stationdetails', {
-            "stationdetails" : details
-        });
-        
-        
 
- });
- 
-
-  });
+    
 
 module.exports = router;
- 
 
 /* GET Station Table page. */
 router.get('/stationtable', function(req, res) {
            
          
            
-           StationTable.find({}, {"id":1, "name":1, "lat":1, "longd":1} ,function(err,stationtable){
+           StationTable.find({}, {} ,function(err,stationtable){
                            
                            res.render('stationtable', {
                                       "stationtable" : stationtable
